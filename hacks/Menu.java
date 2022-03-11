22431a3db2d881dc84dcfdcb666e209c2a78164e
John Mortensen <54686997+jm1021@users.noreply.github.com>
2022-03-11 16:00:11 +0000

    add another option

--- hacks/Menu.java
+++ hacks/Menu.java
@@ -104,7 +104,9 @@ class Main {
         MenuRow[] rows = new MenuRow[]{
                 new MenuRow("Exit", () -> main(null)),
                 new MenuRow("Swap", () -> IntByReference.main(null)),  // lambda style, () -> to point to Class.Method
-                new MenuRow("Matrix", () -> Matrix.main(null))
+                new MenuRow("Matrix", () -> Matrix.main(null)),
+                new MenuRow("Number", () -> Number.main(null))
+
         };
 
         // Menu construction
