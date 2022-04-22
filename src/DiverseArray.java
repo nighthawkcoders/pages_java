ec173ace03f44c7040186080c28080b9b9fc9c3c
John Mortensen <jmortensen@powayusd.com>
2022-04-22 04:51:23 -0700

    2015 Practice Exam add comments

--- src/DiverseArray.java
+++ src/DiverseArray.java
@@ -1,30 +1,36 @@
 package src;
 
-public class RowSummer {
+public class DiverseArray {
     public static int arraySum(int[] arr) {
         int sum = 0;
+
+        // enhanced for loop as values are needed, not index
         for (int num : arr) {
             sum += num;
             System.out.print(num + "\t");  // debug
         }
+
         return sum;
     }
 
-    public static int[] rowSum(int[][] arr2D) {
+    public static int[] rowSums(int[][] arr2D) {
         int rows = arr2D.length;
         int[] sumList = new int[rows];
 
+        // conventional for loop as index used for sumList
         for (int i = 0; i < rows; i++) {
             sumList[i] = arraySum(arr2D[i]);
-            System.out.println(sumList[i]);  // debug
+            System.out.println(" = \t" + sumList[i]);  // debug
         }
 
         return sumList;
     }
 
    public static boolean isDiverse(int[][] arr2D) {
-       int [] sums = rowSum(arr2D);
+       int [] sums = rowSums(arr2D);
        int sumsLength = sums.length;
+
+       // ij loop, two indexes needed in evaluation, similar to bubble sort iteration
        for(int i = 0; i < sumsLength - 1; i++) {
            for (int j = i + 1; j < sumsLength; j++) {
                if (sums[i] == sums[j]) {
